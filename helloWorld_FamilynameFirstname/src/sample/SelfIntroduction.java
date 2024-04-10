63893cee1e1169e5807a1378baca77a393fa4d9e
azumamiyu-27 <azuma.mmm27@gmail.com>
2024-04-11 01:45:51 +0900

    Java1.3

---
+++ helloWorld_FamilynameFirstname/src/sample/SelfIntroduction.java
@@ -0,0 +1,23 @@
+package sample;
+
+public class SelfIntroduction {
+
+	public static void main(String[] args) {
+		// 名前の変数を定義
+		String name = "東美優";
+		
+		// 年齢の変数を定義
+		int age = 27;
+		
+		// 趣味の変数を定義
+		String hobby = "映画鑑賞";
+		
+		/*
+		* 自己紹介文を出力する処理
+		*/
+		System.out.println("私の名前は" + name + "です。");
+		System.out.println("年齢は" + age + "歳です。");
+		System.out.println("趣味は" + hobby + "です。");
+	}
+
+}
